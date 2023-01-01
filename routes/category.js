c0b18c2199aae38b8024b85e8f8ec989e454a328
Tridiktya Putra <tridiktya@gmail.com>
2023-01-01 17:43:39 +0700

    fix merge caused by conflict

--- routes/category.js
+++ routes/category.js
@@ -1,2 +1,9 @@
+const express = require('express');
+
+const CategoryController = require('../controllers/category')
+
+const router = express.Router();
+
+router.get('/category/:category', CategoryController.getSpecificTasks)
 
 module.exports = router;
